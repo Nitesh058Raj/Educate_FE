1d49b59061168cbfa7f46db74b11434939d9789e
WinayGupta777 <vinay.gupta8101@gmail.com>
2023-07-02 23:13:07 +0530

    Created new component 'header'; Included in shared module;

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { HeaderComponent } from './components/header/header.component';
 import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
-  declarations: [LoginComponent],
+  declarations: [LoginComponent, HeaderComponent],
   imports: [CommonModule],
-  exports: [LoginComponent],
+  exports: [LoginComponent, HeaderComponent],
 })
 export class SharedModule {}
