51f29014c67f12c228df644f5da8e2558ff3b693
WinayGupta777 <vinay.gupta8101@gmail.com>
2023-07-03 18:01:51 +0530

    Created 'sidebar' component; Implementation UI and styling; Written logic for changing 'SideBarItem';

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { LoginComponent } from './components/login/login.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [LoginComponent, HeaderComponent],
+  declarations: [LoginComponent, HeaderComponent, SidebarComponent],
   imports: [CommonModule],
-  exports: [LoginComponent, HeaderComponent],
+  exports: [LoginComponent, HeaderComponent, SidebarComponent],
 })
 export class SharedModule {}
