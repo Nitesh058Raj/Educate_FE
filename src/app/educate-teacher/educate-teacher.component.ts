c20ee5b323a48bc57d4bc806a4c94a2f22578f13
WinayGupta777 <vinay.gupta8101@gmail.com>
2023-07-03 18:02:32 +0530

    Used 'sidebar' component in 'educate-teacher' template file;

--- src/app/educate-teacher/educate-teacher.component.ts
+++ src/app/educate-teacher/educate-teacher.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SideBarItemInterface } from '../models/common.model';
 
 @Component({
   selector: 'app-educate-teacher',
@@ -6,7 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./educate-teacher.component.scss'],
 })
 export class EducateTeacherComponent implements OnInit {
+  activeTab: string = 'Dashboard';
+  itemList: SideBarItemInterface[] = [
+    {
+      title: 'Dashboard',
+      label: 'dashboard',
+    },
+    {
+      title: 'My Classes',
+      label: 'my-classes',
+    },
+    {
+      title: 'Assignments',
+      label: 'assignments',
+    },
+  ];
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  navigateToSelectedTab(value: string) {
+    console.log('Navigating to: ', value);
+    this.activeTab = value;
+    // TODO: Write code to change route to the selected tab
+  }
 }
