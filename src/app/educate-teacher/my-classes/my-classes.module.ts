22e7463e5cd4dd49e1c348f18f585a2b0f30c12d
Nitesh <05niteshraj@gmail.com>
2023-07-14 04:34:59 +0530

    Imported and Exported class-list component;

--- src/app/educate-teacher/my-classes/my-classes.module.ts
+++ src/app/educate-teacher/my-classes/my-classes.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { ClassListComponent } from './components/class-list/class-list.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [ClassListComponent],
+  imports: [CommonModule, SharedModule],
+  exports: [ClassListComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class MyClassesModule { }
+export class MyClassesModule {}
