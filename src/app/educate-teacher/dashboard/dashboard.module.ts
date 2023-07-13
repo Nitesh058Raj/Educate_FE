a9d133b0aedf28af327283c39c05a1256aae3f89
Nitesh <05niteshraj@gmail.com>
2023-07-14 02:57:53 +0530

    Imported school-details component and added to html;

--- src/app/educate-teacher/dashboard/dashboard.module.ts
+++ src/app/educate-teacher/dashboard/dashboard.module.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { AnnouncementsComponent } from './components/announcements/announcements.component';
+import { SchoolDetailsComponent } from './components/school-details/school-details.component';
 
 @NgModule({
-  declarations: [AnnouncementsComponent],
+  declarations: [AnnouncementsComponent, SchoolDetailsComponent],
   imports: [CommonModule, SharedModule],
-  exports: [AnnouncementsComponent],
+  exports: [AnnouncementsComponent, SchoolDetailsComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class DashboardModule {}
