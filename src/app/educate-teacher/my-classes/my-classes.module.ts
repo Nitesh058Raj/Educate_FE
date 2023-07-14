feeb88cbb75bfdb453fa77615539de4f15c7f04f
Nitesh <05niteshraj@gmail.com>
2023-07-14 06:29:08 +0530

    Imported and Exported class-details component;

--- src/app/educate-teacher/my-classes/my-classes.module.ts
+++ src/app/educate-teacher/my-classes/my-classes.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { ClassDetailsComponent } from './components/class-details/class-details.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [ClassDetailsComponent],
+  imports: [CommonModule, SharedModule],
+  exports: [ClassDetailsComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class MyClassesModule { }
+export class MyClassesModule {}
