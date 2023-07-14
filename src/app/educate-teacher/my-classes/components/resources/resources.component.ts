89e37a05c37c7fd32b598013cd9f6d0b96f7a536
WinayGupta777 <vinay.gupta8101@gmail.com>
2023-07-14 23:24:42 +0530

    Directive modified; Minor changes;

--- src/app/educate-teacher/my-classes/components/resources/resources.component.ts
+++ src/app/educate-teacher/my-classes/components/resources/resources.component.ts
@@ -3,15 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-resources',
   templateUrl: './resources.component.html',
-  styleUrls: ['./resources.component.scss']
+  styleUrls: ['./resources.component.scss'],
 })
 export class ResourcesComponent implements OnInit {
+  isResourceAvailable = false;
 
-  resources!: string;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
