fc82405b74641e0e57fd7883fdb39fe93b196efe
Nitesh <05niteshraj@gmail.com>
2023-07-15 04:53:40 +0530

    Added FaqComponent in help-and-support;

--- src/app/educate-teacher/help-and-supports/help-and-supports.module.ts
+++ src/app/educate-teacher/help-and-supports/help-and-supports.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { FaqComponent } from './components/faq/faq.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [FaqComponent],
+  imports: [CommonModule, SharedModule],
+  exports: [FaqComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class HelpAndSupportsModule { }
+export class HelpAndSupportsModule {}
