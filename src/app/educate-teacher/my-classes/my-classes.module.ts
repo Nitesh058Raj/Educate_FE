97eca3e4a288987328e3b3ffa1723863a1ce93df
Nitesh Raj <65148119+Nitesh058Raj@users.noreply.github.com>
2023-07-14 23:04:36 +0530

    Create class details component in my classes (#12)
    * Imported and Exported class-details component;
    
    * Created class-details component;
    
    * Directive modified; Layout improved; Removed unecessary code;
    
    * #noissue - Minor css change;
    
    ---------
    
    Co-authored-by: Nitesh <05niteshraj@gmail.com>
    Co-authored-by: WinayGupta777 <vinay.gupta8101@gmail.com>

--- src/app/educate-teacher/my-classes/my-classes.module.ts
+++ src/app/educate-teacher/my-classes/my-classes.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { ClassDetailsComponent } from './components/class-details/class-details.component';
 import { ClassListComponent } from './components/class-list/class-list.component';
 
 @NgModule({
-  declarations: [ClassListComponent],
+  declarations: [ClassListComponent, ClassDetailsComponent],
   imports: [CommonModule, SharedModule],
-  exports: [ClassListComponent],
+  exports: [ClassListComponent, ClassDetailsComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class MyClassesModule {}
