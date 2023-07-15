77478be2aa42b8a1ee7cf75a1e612fd36e04a748
WinayGupta777 <vinay.gupta8101@gmail.com>
2023-07-15 14:59:06 +0530

    Style applied to dropdown; Svg files added; Modified component file; Added test case;

--- src/app/educate-teacher/assignments/assignments.module.ts
+++ src/app/educate-teacher/assignments/assignments.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { AssignmentsOverviewComponent } from './components/assignments-overview/assignments-overview.component';
 
 @NgModule({
   declarations: [AssignmentsOverviewComponent],
-  imports: [CommonModule, SharedModule],
+  imports: [CommonModule, SharedModule, FormsModule],
   exports: [AssignmentsOverviewComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
