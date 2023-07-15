29e2bda23835e7f0b6420f5699a8d8617989e136
Nitesh Raj <65148119+Nitesh058Raj@users.noreply.github.com>
2023-07-15 11:40:35 +0530

    Added contact details component. (#15)
    * Added contact details component;
    
    * Added Icon in header;
    
    * #noissue - Added svg into assets dir; Modified css;
    
    ---------
    
    Co-authored-by: Nitesh <05niteshraj@gmail.com>
    Co-authored-by: WinayGupta777 <vinay.gupta8101@gmail.com>

--- src/app/educate-teacher/help-and-supports/help-and-supports.module.ts
+++ src/app/educate-teacher/help-and-supports/help-and-supports.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { ContactDetailsComponent } from './components/contact-details/contact-details.component';
 import { FaqComponent } from './components/faq/faq.component';
 
 @NgModule({
-  declarations: [FaqComponent],
+  declarations: [FaqComponent, ContactDetailsComponent],
   imports: [CommonModule, SharedModule],
-  exports: [FaqComponent],
+  exports: [FaqComponent, ContactDetailsComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class HelpAndSupportsModule {}
