15bf113fb1b1a3998276d8b5db01e70bc9d48350
Nitesh <05niteshraj@gmail.com>
2023-07-23 04:19:47 +0530

    Implemented popup-modal for add and remove buttons;

--- src/app/educate-teacher/my-classes/components/resources/resources.component.ts
+++ src/app/educate-teacher/my-classes/components/resources/resources.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalService } from 'src/app/shared/services/modal-service/modal.service';
 
 @Component({
   selector: 'app-resources',
@@ -8,7 +9,38 @@ import { Component, OnInit } from '@angular/core';
 export class ResourcesComponent implements OnInit {
   isResourceAvailable = false;
 
-  constructor() {}
+  //pop up variables
+  AddResourceName: string = '';
+  AddResourceURL: string = '';
+
+  constructor(private modalService: ModalService) {}
 
   ngOnInit(): void {}
+
+  openModal(id: string) {
+    this.modalService.open(id);
+  }
+
+  closeModal(id: string) {
+    this.modalService.close(id);
+  }
+
+  handleOnClickEvent(event: any, modalId: string) {
+    switch (event) {
+      case 'Cancel':
+        this.closeModal(modalId);
+        break;
+      case 'Submit':
+        // TODO: Write logic for calling the API of adding Resource
+        // values are bind with AddResourceName and AddResourceURL variables
+
+        this.closeModal(modalId);
+        break;
+      case 'Remove':
+        this.closeModal(modalId);
+        break;
+      default:
+        break;
+    }
+  }
 }
