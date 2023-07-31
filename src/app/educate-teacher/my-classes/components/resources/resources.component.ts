2868f471408bbaf86de1ee7b9a12857eab719ff8
Nitesh <05niteshraj@gmail.com>
2023-08-01 02:37:32 +0530

    Added resource listing feature;

--- src/app/educate-teacher/my-classes/components/resources/resources.component.ts
+++ src/app/educate-teacher/my-classes/components/resources/resources.component.ts
@@ -7,7 +7,71 @@ import { ModalService } from 'src/app/shared/services/modal-service/modal.servic
   styleUrls: ['./resources.component.scss'],
 })
 export class ResourcesComponent implements OnInit {
-  isResourceAvailable = false;
+  isResourceAvailable = true;
+
+  // resources list
+  resourcesList = [
+    {
+      name: 'Maths Textbook',
+      url: 'https://www.example.com/maths-textbook',
+    },
+    {
+      name: 'History Article',
+      url: 'https://www.example.com/history-article',
+    },
+    {
+      name: 'English Video',
+      url: 'https://www.example.com/english-video',
+    },
+    {
+      name: 'Science Website',
+      url: 'https://www.example.com/science-website',
+    },
+    {
+      name: 'Geography Article',
+      url: 'https://www.example.com/geography-article',
+    },
+    {
+      name: 'Physics Textbook',
+      url: 'https://www.example.com/physics-textbook',
+    },
+    {
+      name: 'Chemistry Video',
+      url: 'https://www.example.com/chemistry-video',
+    },
+    {
+      name: 'Biology Website',
+      url: 'https://www.example.com/biology-website',
+    },
+    {
+      name: 'Literature Article',
+      url: 'https://www.example.com/literature-article',
+    },
+    {
+      name: 'Art Video',
+      url: 'https://www.example.com/art-video',
+    },
+    {
+      name: 'Music Textbook',
+      url: 'https://www.example.com/music-textbook',
+    },
+    {
+      name: 'Computer Science Website',
+      url: 'https://www.example.com/computer-science-website',
+    },
+    {
+      name: 'Economics Article',
+      url: 'https://www.example.com/economics-article',
+    },
+    {
+      name: 'Politics Video',
+      url: 'https://www.example.com/politics-video',
+    },
+    {
+      name: 'Psychology Textbook',
+      url: 'https://www.example.com/psychology-textbook',
+    },
+  ];
 
   //pop up variables
   AddResourceName: string = '';
