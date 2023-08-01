07773af696fa3c737a1bdfe48ddf92acee63517e
WinayGupta777 <vinay.gupta8101@gmail.com>
2023-08-01 11:45:58 +0530

    Added SchoolDetailsResponseInterface and SchoolDetailsInterface in model file;

--- src/app/models/common.model.ts
+++ src/app/models/common.model.ts
@@ -2,3 +2,15 @@ export interface SideBarItemInterface {
   title: string;
   label: string;
 }
+
+export interface SchoolDetailsInterface {
+  schoolName: string;
+  schoolAddress: string;
+}
+
+export interface SchoolDetailsResponseInterface {
+  message: string;
+  statusCode: number;
+  status: string;
+  data: SchoolDetailsInterface[];
+}
