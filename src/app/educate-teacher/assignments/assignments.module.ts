80776d22925f5f1488b502fad0d075fae38140fd
WinayGupta777 <vinay.gupta8101@gmail.com>
2023-08-01 21:47:52 +0530

    Created 'AssignmentsList' component; Modified Styles;

--- src/app/educate-teacher/assignments/assignments.module.ts
+++ src/app/educate-teacher/assignments/assignments.module.ts
@@ -2,12 +2,13 @@ import { CommonModule } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { AssignmentsListComponent } from './components/assignments-list/assignments-list.component';
 import { AssignmentsOverviewComponent } from './components/assignments-overview/assignments-overview.component';
 
 @NgModule({
-  declarations: [AssignmentsOverviewComponent],
+  declarations: [AssignmentsOverviewComponent, AssignmentsListComponent],
   imports: [CommonModule, SharedModule, FormsModule],
-  exports: [AssignmentsOverviewComponent],
+  exports: [AssignmentsOverviewComponent, AssignmentsListComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AssignmentsModule {}
---
+++ src/app/educate-teacher/assignments/components/assignments-list/assignments-list.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-assignments-list',
+  templateUrl: './assignments-list.component.html',
+  styleUrls: ['./assignments-list.component.scss'],
+})
+export class AssignmentsListComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
