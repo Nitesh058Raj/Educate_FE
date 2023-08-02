448fcc017c71baf1d3b85b52a86423a1f8ea9dbf
WinayGupta777 <vinay.gupta8101@gmail.com>
2023-08-02 22:44:35 +0530

    Added table in template file; Added styling;

--- src/app/educate-teacher/assignments/components/assignments-list/assignments-list.component.ts
+++ src/app/educate-teacher/assignments/components/assignments-list/assignments-list.component.ts
@@ -6,6 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./assignments-list.component.scss'],
 })
 export class AssignmentsListComponent implements OnInit {
+  // TODO: Remove this dummy data
+  assignmentDetails: {
+    id: number;
+    title: string;
+    dueDate: string;
+    status: string;
+  }[] = [
+    {
+      id: 1,
+      title: 'Assignment 1',
+      dueDate: '2023-07-25',
+      status: 'Active',
+    },
+    {
+      id: 2,
+      title: 'Assignment 2',
+      dueDate: '2023-08-05',
+      status: 'Active',
+    },
+    {
+      id: 3,
+      title: 'Assignment 3',
+      dueDate: '2023-10-25',
+      status: 'Inactive',
+    },
+  ];
+
   constructor() {}
 
   ngOnInit(): void {}
