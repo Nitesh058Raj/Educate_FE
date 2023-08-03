2d670a012614eca044ede48443aaf20042bda749
Nitesh <05niteshraj@gmail.com>
2023-08-03 05:36:44 +0530

    noissue - change HttpClient to HttpClientModule;

--- src/app/educate-teacher/my-classes/components/class-list/class-list.component.spec.ts
+++ src/app/educate-teacher/my-classes/components/class-list/class-list.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { ClassListComponent } from './class-list.component';
 
 describe('ClassListComponent', () => {
@@ -9,7 +9,7 @@ describe('ClassListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HttpClient],
+      imports: [HttpClientModule],
       declarations: [ClassListComponent],
     }).compileComponents();
   });
