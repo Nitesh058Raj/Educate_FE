e96e31f1b9efb4e209388f38c849a6d72f4d912d
WinayGupta777 <vinay.gupta8101@gmail.com>
2023-08-03 15:18:40 +0530

    Created new component `AssignmentSingleComponent`; Added form in template file; Added Styles;

---
+++ src/app/educate-teacher/assignments/components/assignment-single/assignment-single.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-assignment-single',
+  templateUrl: './assignment-single.component.html',
+  styleUrls: ['./assignment-single.component.scss'],
+})
+export class AssignmentSingleComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
