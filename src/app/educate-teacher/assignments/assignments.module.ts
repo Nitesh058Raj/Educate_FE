55e9a9aa17c242297611adaa0df0a190100aadcd
Nitesh <05niteshraj@gmail.com>
2023-08-04 05:32:23 +0530

    Exported AssignmentClassComponent;

--- src/app/educate-teacher/assignments/assignments.module.ts
+++ src/app/educate-teacher/assignments/assignments.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { AssignmentClassComponent } from './components/assignment-class/assignment-class.component';
 import { AssignmentSingleComponent } from './components/assignment-single/assignment-single.component';
 import { AssignmentsListComponent } from './components/assignments-list/assignments-list.component';
 import { AssignmentsOverviewComponent } from './components/assignments-overview/assignments-overview.component';
@@ -11,12 +12,14 @@ import { AssignmentsOverviewComponent } from './components/assignments-overview/
     AssignmentsOverviewComponent,
     AssignmentsListComponent,
     AssignmentSingleComponent,
+    AssignmentClassComponent,
   ],
   imports: [CommonModule, SharedModule, FormsModule],
   exports: [
     AssignmentsOverviewComponent,
     AssignmentsListComponent,
     AssignmentSingleComponent,
+    AssignmentClassComponent,
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
