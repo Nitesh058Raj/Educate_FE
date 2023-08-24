8dfa7a9ab8c39fda612442c3b7bb89f14ee69b86
Nitesh <05niteshraj@gmail.com>
2023-08-24 18:05:00 +0530

    Initialized NgRx store annd Effect;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
+
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from 'src/environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EducateTeacherRoutingModule } from './educate-teacher/educate-teacher-routing.module';
@@ -19,6 +24,13 @@ import { SharedModule } from './shared/shared.module';
     NotFoundModule,
     NotFoundRoutingModule,
     SharedModule,
+    StoreModule.forRoot({}, {}),
+    StoreDevtoolsModule.instrument({
+      name: 'Educate Teacher App DevTools',
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
+    EffectsModule.forRoot([]),
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/educate-teacher/educate-teacher.module.ts
+++ src/app/educate-teacher/educate-teacher.module.ts
@@ -1,6 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+
 import { SharedModule } from '../shared/shared.module';
 import { AssignmentsComponent } from './assignments/assignments.component';
 import { AssignmentsModule } from './assignments/assignments.module';
@@ -12,6 +15,8 @@ import { HelpAndSupportsComponent } from './help-and-supports/help-and-supports.
 import { HelpAndSupportsModule } from './help-and-supports/help-and-supports.module';
 import { MyClassesComponent } from './my-classes/my-classes.component';
 import { MyClassesModule } from './my-classes/my-classes.module';
+import { MyClassesEffects } from './my-classes/state/effects/my-classes.effects';
+import { myClassesReducer } from './my-classes/state/reducers/my-classes.reducers';
 import { EducateTeacherService } from './services/educate-teacher.service';
 
 @NgModule({
@@ -31,6 +36,8 @@ import { EducateTeacherService } from './services/educate-teacher.service';
     AssignmentsModule,
     HelpAndSupportsModule,
     HttpClientModule,
+    StoreModule.forFeature('myClasses', myClassesReducer),
+    EffectsModule.forFeature([MyClassesEffects]),
   ],
   providers: [EducateTeacherService],
   exports: [EducateTeacherComponent],
