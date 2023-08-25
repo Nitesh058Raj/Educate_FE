e7ed95d48474b1dc586425bb9d043d7fd58e2d33
Nitesh <05niteshraj@gmail.com>
2023-08-25 19:35:57 +0530

    Added required actions;

--- src/app/educate-teacher/my-classes/state/actions/class-details.actions.spec.ts
+++ src/app/educate-teacher/my-classes/state/actions/class-details.actions.spec.ts
@@ -1,8 +1,33 @@
-import { ClassDetailsActions } from '.';
+import { ClassDetailsInterface } from 'src/app/models/common.model';
+import * as fromClassDetailsActions from './class-details.actions';
 
 describe('Class Details Actions', () => {
-  it('should create Load Class Details action', () => {
-    const action = ClassDetailsActions.loadClassDetails();
-    expect(action.type).toBe('[Class Details] Load Class Details');
+  it('should create the loadClassDetails action', () => {
+    const classId = 123;
+    const action = fromClassDetailsActions.loadClassDetails({ classId });
+
+    expect(action.type).toEqual('[Class Details] Load Class Details');
+    expect(action.classId).toEqual(classId);
+  });
+
+  it('should create the loadClassDetailsSuccess action', () => {
+    const classDetails: ClassDetailsInterface = {
+      className: 'Class Name',
+      classDescription: 'Class Description',
+    };
+    const action = fromClassDetailsActions.loadClassDetailsSuccess({
+      classDetails,
+    });
+
+    expect(action.type).toEqual('[Class Details] Load Class Details Success');
+    expect(action.classDetails).toEqual(classDetails);
+  });
+
+  it('should create the loadClassDetailsFailure action', () => {
+    const error = 'An error occurred';
+    const action = fromClassDetailsActions.loadClassDetailsFailure({ error });
+
+    expect(action.type).toEqual('[Class Details] Load Class Details Failure');
+    expect(action.error).toEqual(error);
   });
 });
--- src/app/educate-teacher/my-classes/state/actions/class-details.actions.ts
+++ src/app/educate-teacher/my-classes/state/actions/class-details.actions.ts
@@ -1,3 +1,17 @@
-import { createAction } from '@ngrx/store';
+import { createAction, props } from '@ngrx/store';
+import { ClassDetailsInterface } from 'src/app/models/common.model';
 
-export const loadClassDetails = createAction('[Class Details] Load Class Details');
+export const loadClassDetails = createAction(
+  '[Class Details] Load Class Details',
+  props<{ classId: number }>()
+);
+
+export const loadClassDetailsSuccess = createAction(
+  '[Class Details] Load Class Details Success',
+  props<{ classDetails: ClassDetailsInterface }>()
+);
+
+export const loadClassDetailsFailure = createAction(
+  '[Class Details] Load Class Details Failure',
+  props<{ error: string }>()
+);
