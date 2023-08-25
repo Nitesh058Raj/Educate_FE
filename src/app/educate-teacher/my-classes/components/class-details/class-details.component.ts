7fca909d8c39b5484694c5d4bbc9d5cb295cb107
Nitesh Raj <65148119+Nitesh058Raj@users.noreply.github.com>
2023-08-25 12:55:58 +0530

    Noissue/editing display component logic in my classes (#37)
    * Added displayComponent logic;
    
    * Added displayComponent logic;
    
    * Added displayComponent logic;
    
    * Edited for minor change in html logic;
    
    * Added displayComponent logic;
    
    ---------
    
    Co-authored-by: Nitesh <05niteshraj@gmail.com>

--- src/app/educate-teacher/my-classes/components/class-details/class-details.component.ts
+++ src/app/educate-teacher/my-classes/components/class-details/class-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { EMPTY, Observable, catchError, finalize, map, tap } from 'rxjs';
 import { EducateTeacherService } from 'src/app/educate-teacher/services/educate-teacher.service';
 import { ClassDetailsInterface } from 'src/app/models/common.model';
@@ -10,6 +10,8 @@ import { ModalService } from 'src/app/shared/services/modal-service/modal.servic
   styleUrls: ['./class-details.component.scss'],
 })
 export class ClassDetailsComponent implements OnInit {
+  @Input() displayComponent: boolean | null = false;
+
   isLoading = true;
   errorMessage = '';
   classDetails$: Observable<ClassDetailsInterface> | undefined;
--- src/app/educate-teacher/my-classes/components/class-roster/class-roster.component.ts
+++ src/app/educate-teacher/my-classes/components/class-roster/class-roster.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-class-roster',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./class-roster.component.scss'],
 })
 export class ClassRosterComponent implements OnInit {
+  @Input() displayComponent: boolean | null = false;
+
   studentDetails: {
     id: number;
     name: string;
