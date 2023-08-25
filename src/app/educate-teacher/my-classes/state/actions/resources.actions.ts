c822ae5612137c3447e57f9727d5704d57ef796b
Nitesh <05niteshraj@gmail.com>
2023-08-25 19:36:10 +0530

    Added required actions;

--- src/app/educate-teacher/my-classes/state/actions/resources.actions.ts
+++ src/app/educate-teacher/my-classes/state/actions/resources.actions.ts
@@ -1,3 +1,17 @@
-import { createAction } from '@ngrx/store';
+import { createAction, props } from '@ngrx/store';
+import { ResourcesInterface } from 'src/app/models/common.model';
 
-export const loadResources = createAction('[Resources] Load Resources');
+export const loadResources = createAction(
+  '[Resources] Load Resources',
+  props<{ classId: number }>()
+);
+
+export const loadResourcesSuccess = createAction(
+  '[Resources] Load Resources Success',
+  props<{ resources: ResourcesInterface[] }>()
+);
+
+export const loadResourcesFailure = createAction(
+  '[Resources] Load Resources Failure',
+  props<{ error: string }>()
+);
