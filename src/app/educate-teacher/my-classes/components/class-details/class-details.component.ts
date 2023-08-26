48b5ef7b12b74c2885df383ba31b987cf153196d
Nitesh <05niteshraj@gmail.com>
2023-08-26 14:51:09 +0530

    Added update class details logic in component;

--- src/app/educate-teacher/my-classes/components/class-details/class-details.component.ts
+++ src/app/educate-teacher/my-classes/components/class-details/class-details.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { ClassDetailsInterface } from 'src/app/models/common.model';
 import { ModalService } from 'src/app/shared/services/modal-service/modal.service';
 import { State } from '../../state';
+import { ClassDetailsActions } from '../../state/actions';
 import {
   ClassDetailsSelectors,
   ClassListSelectors,
@@ -19,14 +20,23 @@ export class ClassDetailsComponent implements OnInit {
   isLoading$: Observable<boolean> | undefined;
   errorMessage$: Observable<string | null> | null = null;
   classDetails$: Observable<ClassDetailsInterface | null> | undefined;
+  selectedClassId$: Observable<number | null> | undefined;
 
   // Edit popup variables
-  editclassName: string = '';
-  editclassDescription: string = '';
+  initclassName: string = '';
+  initclassDescription: string = '';
+  editedclassName: string = '';
+  editedclassDescription: string = '';
+  errorMessagePopUp$: Observable<string | null> | null = null;
+
+  // temp vars
+  tempClassId: number | null = null;
+  errorMessagePopup: string | null = null;
 
   constructor(
     private modalService: ModalService,
-    private store: Store<State>
+    private store: Store<State>,
+    private cdr: ChangeDetectorRef
   ) {}
 
   ngOnInit(): void {
@@ -37,12 +47,58 @@ export class ClassDetailsComponent implements OnInit {
     this.classDetails$ = this.store.select(
       ClassDetailsSelectors.getClassDetails
     );
+
+    this.classDetails$.subscribe((data) => {
+      if (data) {
+        this.initclassName = data.className;
+        this.initclassDescription = data.classDescription;
+
+        this.editedclassName = this.initclassName;
+        this.editedclassDescription = this.initclassDescription;
+      }
+    });
+
     this.isLoading$ = this.store.select(ClassDetailsSelectors.getLoading);
+
+    this.selectedClassId$ = this.store.select(
+      ClassListSelectors.getCurrentClassId
+    );
+
+    this.selectedClassId$.subscribe((data) => {
+      if (data) {
+        this.tempClassId = data;
+      }
+    });
 
     // errorMessage for class details component is not required
     this.errorMessage$ = this.store.select(
       ClassListSelectors.getClassListError
     );
+
+    this.errorMessagePopUp$ = this.store.select(ClassDetailsSelectors.getError);
+
+    this.errorMessagePopUp$.subscribe((data) => {
+      if (data) {
+        this.errorMessagePopup = data;
+        this.cdr.detectChanges();
+      }
+    });
+  }
+
+  isEnableEditButton() {
+    if (
+      this.initclassName === this.editedclassName &&
+      this.initclassDescription === this.editedclassDescription
+    ) {
+      return true;
+    } else if (
+      this.editedclassName === '' ||
+      this.editedclassDescription === ''
+    ) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   openModal(id: string) {
@@ -59,10 +115,19 @@ export class ClassDetailsComponent implements OnInit {
         this.closeModal(modalId);
         break;
       case 'Update':
-        // TODO: Write logic for calling the API of adding Class
-        // values are bind to the variables editclassName and editclassDescription with ngModel
+        this.store.dispatch(
+          ClassDetailsActions.updateClassDetails({
+            classDetails: {
+              classId: this.tempClassId,
+              className: this.editedclassName,
+              classDescription: this.editedclassDescription,
+            },
+          })
+        );
 
-        this.closeModal(modalId);
+        if (this.errorMessagePopUp$ == null) {
+          this.closeModal(modalId);
+        }
         break;
       default:
         break;
