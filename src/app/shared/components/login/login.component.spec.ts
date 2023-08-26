0e3a7fd1fdc73f3c11f825ef2c15f025ac57d694
WinayGupta777 <vinay.gupta8101@gmail.com>
2023-08-26 22:25:41 +0530

    #noissue - Fixed failing test cases;

--- src/app/shared/components/login/login.component.spec.ts
+++ src/app/shared/components/login/login.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { FormsModule } from '@angular/forms';
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
@@ -8,9 +9,9 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
-    })
-    .compileComponents();
+      declarations: [LoginComponent],
+      imports: [FormsModule],
+    }).compileComponents();
   });
 
   beforeEach(() => {
