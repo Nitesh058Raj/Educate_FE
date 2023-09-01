cd1785b4eaba3ff061033decda13c396073ab1bc
Nitesh <05niteshraj@gmail.com>
2023-09-02 02:28:42 +0530

    Added Adding resources and removing resources functionallity;

--- src/app/educate-teacher/my-classes/components/resources/resources.component.spec.ts
+++ src/app/educate-teacher/my-classes/components/resources/resources.component.spec.ts
@@ -1,16 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HttpClientModule } from '@angular/common/http';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { Actions } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
+import { of } from 'rxjs';
+import { ModalService } from 'src/app/shared/services/modal-service/modal.service';
 import { ResourcesComponent } from './resources.component';
 
 describe('ResourcesComponent', () => {
   let component: ResourcesComponent;
   let fixture: ComponentFixture<ResourcesComponent>;
+  let mockAction: any;
 
   beforeEach(async () => {
+    mockAction = of({ type: '[Resources] Load Resources' });
     await TestBed.configureTestingModule({
-      imports: [HttpClientModule, StoreModule.forRoot({})],
+      imports: [HttpClientModule, StoreModule.forRoot({}), ReactiveFormsModule],
+      providers: [
+        ModalService,
+        FormBuilder,
+        { provide: Actions, useValue: mockAction },
+      ],
       declarations: [ResourcesComponent],
     }).compileComponents();
   });
